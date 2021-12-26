e4e5b51e223ff0e7bca8a714daf109661292091b
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-12-26 16:23:02 +0100

    create inital character

---
+++ src/hooks/useStoryteller/InitialCharacter.object.tsx
@@ -0,0 +1,55 @@
+﻿import {
+	AssetType,
+	Attribute,
+	Character,
+	IAssetInstance,
+} from 'lib-storyteller';
+import { CharacterMapEntry } from 'lib-storyteller/dist/model/character.interface';
+import * as _ from 'lodash';
+
+const InitialCharacter: Character = {
+	name: 'Leonardo',
+	assets: new Map<string, IAssetInstance>([]),
+	attributes: new Map<string, Attribute>([
+		[
+			'Strength',
+			{
+				name: 'Strength',
+				type: AssetType.Attribute,
+				pointsCollected: 1,
+			},
+		],
+		[
+			'Stealth',
+			{
+				name: 'Stealth',
+				type: AssetType.Attribute,
+				pointsCollected: 1,
+			},
+		],
+		[
+			'Intelligence',
+			{
+				name: 'Intelligence',
+				type: AssetType.Attribute,
+				pointsCollected: 1,
+			},
+		],
+	]),
+	equipment: {
+		head: undefined,
+		body: undefined,
+		wrist: undefined,
+		necklace: undefined,
+		shoes: undefined,
+	},
+	map: {
+		currentPlace: 'Reghaven',
+		currentLocation: 'Bus To Reghaven',
+		unlockedLocations: new Map<string, CharacterMapEntry[]>(),
+	},
+};
+
+export function createNewCharacter(): Character {
+	return _.cloneDeep(InitialCharacter);
+}
---
+++ src/hooks/useStoryteller/useStoryteller.tsx
@@ -0,0 +1,9 @@
+﻿import { useState } from 'react';
+import { Character } from 'lib-storyteller';
+import { createNewCharacter } from './InitialCharacter.object';
+
+export function useStoryteller() {
+	const [character, setCharacter] = useState<Character>(createNewCharacter());
+
+	return { character };
+}
