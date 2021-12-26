46aa7a553c2d4c4869142b723ed74ca604c9f7aa
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-12-26 23:42:06 +0100

    add modal for assets

--- src/components/screens/Dashboard/DecisionCard/AssetList/AssetList.tsx
+++ src/components/screens/Dashboard/DecisionCard/AssetList/AssetList.tsx
@@ -1,6 +1,7 @@
 ﻿import { Decision, IAssetInstance } from 'lib-storyteller';
-import React from 'react';
-import { Image, Pressable, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Modal, Pressable, StyleSheet, View, Text } from 'react-native';
+import { RButton } from '../../../../common/RButton';
 
 export function AssetList(props: { decision: Decision }): React.ReactElement {
 	return (
@@ -22,12 +23,71 @@ function AssetEntry(props: {
 	imageUri: string;
 	assetInstance: IAssetInstance;
 }): React.ReactElement {
+	const [visible, setVisible] = useState(false);
+
 	return (
-		<Pressable style={Styles.AssetIcon}>
+		<Pressable style={Styles.AssetIcon} onPress={() => setVisible(true)}>
 			<Image style={Styles.AssetIconImage} source={{ uri: props.imageUri }} />
+			<Modal
+				animationType={'slide'}
+				visible={visible}
+				style={StylesModal.Modal}
+				transparent>
+				<View style={StylesModal.ModalWrapper}>
+					<View style={StylesModal.ModalContent}>
+						<Text style={StylesModal.Title}>{props.assetInstance[0].name}</Text>
+						<Text style={StylesModal.Text}>
+							Required: {props.assetInstance[1]}
+						</Text>
+						<RButton text={'Close'} onPress={() => setVisible(false)} />
+					</View>
+				</View>
+			</Modal>
 		</Pressable>
 	);
 }
+
+const StylesModal = StyleSheet.create({
+	Modal: {
+		display: 'flex',
+		alignContent: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#111',
+	},
+	ModalWrapper: {
+		height: '100%',
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		alignContent: 'center',
+		backfaceVisibility: 'hidden',
+	},
+	ModalContent: {
+		padding: 20,
+		backgroundColor: '#303030',
+		borderRadius: 16,
+		borderStyle: 'solid',
+		borderWidth: 1,
+		borderColor: '#606060',
+		shadowRadius: 1,
+		shadowColor: 'black',
+		shadowOffset: { width: 1, height: 1 },
+		shadowOpacity: 0.5,
+	},
+	Title: {
+		color: 'white',
+		fontSize: 30,
+		fontFamily: 'TwinkleStar-Regular',
+	},
+	Text: {
+		color: 'white',
+		fontSize: 24,
+		fontFamily: 'EBGaramond-VariableFont_wgth',
+	},
+});
 
 const Styles = StyleSheet.create({
 	AssetIcon: {
