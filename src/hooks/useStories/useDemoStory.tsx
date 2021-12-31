3b57edef68af6edfdbb38f2459296f619ebde8ad
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-12-31 19:17:02 +0100

    write demo story

---
+++ src/hooks/useStories/useDemoStory.tsx
@@ -0,0 +1,57 @@
+﻿import { Story, Place } from 'lib-storyteller';
+
+export function useDemoStory() {
+	const reghaven: Place = {
+		name: 'Reghaven',
+		locations: [
+			{
+				name: 'Bus to Reghaven',
+				isUnlockedFromBeginning: true,
+				characterCanLeaveAnytime: false,
+				isVisibleOnMap: false,
+			},
+		],
+	};
+
+	const demoStory: Story = {
+		title: 'The Arrival',
+		text: 'The Bus has seen better days. Dusty seats and windows so dirty you barely can see through hind your sight.',
+		conditionsToShow: {
+			characterHasAssets: [],
+			characterHasNotAssets: [],
+			characterIsAtPlace: reghaven,
+		},
+		decisions: [
+			{
+				title: 'Read Todays Newspaper',
+				text: 'You found it on another seat',
+				attribute: {
+					attributeToActivate: 'Intelligence',
+					attributeLevelFor100Percent: 1,
+				},
+				onWin: {
+					text: "Next to the funnies and some local news about topics you don't understand, there are some interesting title stories.",
+					winDissolvesAssets: [],
+					winResolveAssets: [],
+					grantedAttributePoints: 1,
+				},
+				onFail: {
+					text: 'You remember that you cannot read',
+					failResolveAssets: [],
+					failDissolvesAssets: [],
+				},
+				conditionsToUse: {
+					characterHasAssets: [],
+					characterHasNotAssets: [],
+				},
+				conditionsToShow: {
+					characterHasAssets: [],
+					characterHasNotAssets: [],
+					characterIsAtLocation: 'Bus to Reghaven',
+				},
+			},
+		],
+	};
+
+	return demoStory;
+}
---
+++ src/hooks/useStories/useStories.tsx
@@ -0,0 +1,15 @@
+﻿import { useState } from 'react';
+import { Character, Decision, Story } from 'lib-storyteller';
+import { useDemoStory } from './useDemoStory';
+
+export function useStories() {
+	const demoStory = useDemoStory();
+	const [decisions] = useState<Decision[]>();
+
+	function updateDecisionsForCharacter(character: Character): void {}
+
+	return {
+		decisions,
+		updateDecisionsForCharacter,
+	};
+}
